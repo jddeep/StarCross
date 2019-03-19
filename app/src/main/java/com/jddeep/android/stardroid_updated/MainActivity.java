c75d3e95a15309440ff0a85c90f8384a2bb0a856
jd003walker <41433222+jd003walker@users.noreply.github.com>
2019-03-19 18:56:10 +0530

    Initial commit

---
+++ app/src/main/java/com/jddeep/android/stardroid_updated/MainActivity.java
@@ -0,0 +1,13 @@
+package com.jddeep.android.stardroid_updated;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
